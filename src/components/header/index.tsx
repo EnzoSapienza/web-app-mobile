29b96c1c6609bb94f1bf7a35697bd697872e2272
Daisy Torrico <daisytorrico@hotmail.com>
2026-04-17 17:53:04 -0300

    agregar logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import styles from './style.module.css';
+import logo from '../../assets/logo.svg';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link to="/" className={styles.logo}>THE LUMINAL</Link>
+      <Link to="/" className={styles.logo}><img src={logo} alt="logo" /></Link>
       <nav>
         <Link to="/contact" className={styles.contactBtn}>Contact</Link>
       </nav>
