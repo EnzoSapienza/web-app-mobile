46496807a224e54706696f26d3d7892972d3c2dd
EnzoSapienza <enzosapienzadev@gmail.com>
2026-04-19 17:49:19 -0300

    Añadido BrowserRouter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/web-app-mobile/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
