b4e39dc243223c31982deda8545bf6852e3c9831
EnzoSapienza <enzosapienzadev@gmail.com>
2026-04-19 23:50:58 -0300

    Página de lista de deseos funcional.

--- src/interfaces/LocalItems/WishlistItem.ts
+++ src/interfaces/LocalItems/WishlistItem.ts
@@ -1,9 +1,6 @@
 export default interface WishlistItem {
   id: number;
-  title: string;
-  artist: string;
-  image: string;      // Guardamos la URL para no pedirla a la API cada vez
-  priority: number;   // Campo requerido: Prioridad/Estrellas
-  label: string;      // Campo requerido: Categoría/Etiqueta
-  note?: string;      // Campo opcional: Nota personal (máx 150 carac.)
+  priority: number; // Campo requerido: Prioridad/Estrellas
+  label: string; // Campo requerido: Categoría/Etiqueta
+  note?: string; // Campo opcional: Nota personal (máx 150 carac.)
 }
--- src/pages/Wishlist/index.tsx
+++ src/pages/Wishlist/index.tsx
@@ -1,3 +1,86 @@
+import { useEffect, useState } from 'react';
+import useWishlist from '../../services/local/wishlist';
+import styles from './style.module.css';
+import type Artwork from '../../interfaces/Responses/Artwork';
+import GetArtworksArr from '../../services/api/getArtworksArr';
+import ArtGrid from '../../components/artGrid';
+import ArtCard from '../../components/artCard';
+import { Link } from 'react-router-dom';
+
 export default function Wishlist() {
-  return <h1>Wishlist</h1>;
+  const limit = 10; // 10 cartas por carga
+  const { wishlist, addItem } = useWishlist();
+
+  const [artworks, setArtworks] = useState<(Artwork | undefined)[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [more, setMore] = useState(true);
+
+  // Buscar los artículos
+  useEffect(() => {
+    const fetchArtworks = async () => {
+      setLoading(true);
+      const sliced = wishlist.slice(offset, offset + limit).map((i) => i.id);
+      const result = await GetArtworksArr(sliced);
+
+      // Cargarlos
+      setArtworks((prev) => {
+        const existingIds = new Set(prev.map((a) => a?.id));
+        const filtered = result.filter((a) => !existingIds.has(a?.id));
+        return [...prev, ...filtered];
+      });
+
+      // Liberar
+      setMore(wishlist.length > offset + limit);
+      setLoading(false);
+    };
+
+    fetchArtworks();
+  }, [offset]);
+
+  return (
+    <section className="page-container">
+      <h1 className="page-title">Wishlist</h1>
+
+      {wishlist.length === 0 && !loading ? (
+        <p className={styles.status}>
+          The Wishlist is empty.
+          <br />
+          <Link to="/search" className="btn-gold">
+            Search for your new favorite artwork!
+          </Link>
+        </p>
+      ) : (
+        <>
+          {loading && artworks.length === 0 && (
+            <p className={styles.status}>Exploring archives...</p>
+          )}
+
+          <ArtGrid>
+            {artworks.map((art) => (
+              <ArtCard key={art?.id} art={art} />
+            ))}
+          </ArtGrid>
+
+          {loading && artworks.length > 0 && (
+            <p className={styles.status}>Loading more...</p>
+          )}
+
+          {!loading && (
+            <div className={styles.buttonContainer}>
+              <button
+                disabled={!more}
+                className="btn-gold"
+                onClick={() =>
+                  setOffset((prev) => Math.min(prev + limit, wishlist.length))
+                }
+              >
+                Load more
+              </button>
+            </div>
+          )}
+        </>
+      )}
+    </section>
+  );
 }
