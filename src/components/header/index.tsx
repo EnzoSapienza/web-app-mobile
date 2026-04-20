10245c476e066009c0300f50abf1f58498c40caf
EnzoSapienza <enzosapienzadev@gmail.com>
2026-04-20 01:15:13 -0300

    El header se esconde al scrollear. El navbar lo acompaña si está arriba.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
 import styles from './style.module.css';
 import logo from '../../assets/logo.svg';
+import useScrollDirection from '../../services/useScollDirection';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link to="/" className={styles.logo}><img src={logo} alt="logo" /></Link>
+    <header
+      className={[
+        styles.header,
+        useScrollDirection() === 'down' ? styles.scrollDown : '',
+      ].join(' ')}
+    >
+      <Link to="/" className={styles.logo}>
+        <img src={logo} alt="logo" />
+      </Link>
       <nav>
-        <Link to="/contact" className={styles.contactBtn}>Contact</Link>
+        <Link to="/contact" className={styles.contactBtn}>
+          Contact
+        </Link>
       </nav>
     </header>
   );
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,9 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from './style.module.css';
+import useScrollDirection from '../../services/useScollDirection';
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
+    <nav
+      className={[
+        styles.navbar,
+        useScrollDirection() === 'down' ? styles.scrollDown : '',
+      ].join(' ')}
+    >
       <ul>
         <li>
           <Link to="/">Home</Link>
