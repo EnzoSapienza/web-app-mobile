c4995571b7bd9b75334ec004a9615c2ee8841b99
EnzoSapienza <enzosapienzadev@gmail.com>
2026-04-20 00:40:42 -0300

    Las cartas ahora son robustas ante fallo en la carga de imagen.

--- src/components/artCard/index.tsx
+++ src/components/artCard/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { Link } from 'react-router-dom';
 import styles from './style.module.css';
+import { useState } from 'react';
 
 interface Props {
   art?: {
@@ -11,16 +13,36 @@ interface Props {
 }
 
 export default function ArtCard({ art }: Props) {
-  if (!art) return <></>;
+  if (!art || !art.imageUrl) return <></>;
 
   const displayTitle = art.title || 'Untitled Masterpiece';
   const displayArtist = art.artist_display || 'Unknown Artist';
+
+  const [errorCount, setErrorCount] = useState(0);
+  const handleImageError = (
+    e: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    if (errorCount === 0) {
+      console.log('primer error');
+      e.currentTarget.src = art.imageUrl.replace(/\/full\/\d+,/, '/full/full');
+      setErrorCount(1);
+    } else if (errorCount === 1) {
+      console.log('segundo error');
+      e.currentTarget.src = 'public/placeholder.png';
+      setErrorCount(2);
+    }
+  };
 
   return (
     <Link to={`/details/${art.id}`} className={styles.cardLink}>
       <article className={styles.card}>
         <figure className={styles.figure}>
-          <img src={art.imageUrl} alt={displayTitle} className={styles.image} />
+          <img
+            src={art.imageUrl}
+            alt={displayTitle}
+            className={styles.image}
+            onError={handleImageError}
+          />
         </figure>
         <div className={styles.info}>
           <h3 className={styles.artTitle}>{displayTitle}</h3>
