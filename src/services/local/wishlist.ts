15510373125424210d30a8999a016e2fd48b9a68
EnzoSapienza <enzosapienzadev@gmail.com>
2026-04-20 00:51:45 -0300

    Cambio menor para faciliar añadir a la lista de deseos.

--- src/services/local/wishlist.ts
+++ src/services/local/wishlist.ts
@@ -6,7 +6,9 @@ const KEY = 'wishlist';
 export default function useWishlist() {
   const [wishlist, setWishlist] = useLocalStorage<WishlistItem[]>(KEY, []);
 
-  const addItem = (item: WishlistItem) => {
+  const addToList = (item: WishlistItem | undefined) => {
+    if (!item) return;
+
     setWishlist((prev) => {
       if (prev.some((i) => i.id === item.id)) return prev;
       return [...prev, item];
@@ -19,5 +21,5 @@ export default function useWishlist() {
 
   const isInWishlist = (id: number) => wishlist.some((item) => item.id === id);
 
-  return { wishlist, addItem, removeItem, isInWishlist };
+  return { wishlist, addToList, removeItem, isInWishlist };
 }
