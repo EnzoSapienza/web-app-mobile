63fec886157635bf3b132b654f7600e869b15a13
Daisy Torrico <daisytorrico@hotmail.com>
2026-04-23 21:25:30 -0300

    ajuste del header y se agrega el boton modo oscuro/claro

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 import styles from './style.module.css';
 import logo from '../../assets/logo.svg';
 import useScrollDirection from '../../services/useScollDirection';
+import useTheme from '../../services/local/useTheme';
 
 export default function Header() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <header
       className={[
@@ -14,7 +16,14 @@ export default function Header() {
       <Link to="/" className={styles.logo}>
         <img src={logo} alt="logo" />
       </Link>
-      <nav>
+      <nav className={styles.navGroup}>
+        <button 
+          onClick={toggleTheme} 
+          className={styles.themeBtn} 
+          title="Cambiar a modo oscuro/claro"
+        >
+          {theme === 'light' ? '☾' : '☀'}
+        </button>
         <Link to="/contact" className={styles.contactBtn}>
           Contact
         </Link>
