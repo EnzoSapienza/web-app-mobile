e4a3b13d0f694207b2fa237cb7410b8975244811
EnzoSapienza <enzosapienzadev@gmail.com>
2026-04-26 22:23:33 -0300

    Corrección carga del mapa.

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import styles from './style.module.css';
 import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix íconos Leaflet + Vite
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 export default function Map() {
   const position: [number, number] = [-34.9215, -57.9536];
